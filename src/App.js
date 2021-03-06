818edf91ed684357b84d01137ffdccd842a6fcb4
Tommy Alhamra <tommy.alhamra@gmail.com>
2021-03-06 23:11:21 +0700

    add+ layout design

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  const user = "Tommy Biasa";
-  const userJsx = <strong>Tommy JSX dong</strong>;
   //JSX JavaScript XML bisa mengabungkan tag html
+  const category = "Sneaker";
+  const title = "Puma Classic Suede";
+  const price = 1500;
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <br />
-        {user} - {userJsx}
-      </header>
+    <div class="ParentBox">
+      <div class="Foto">
+        <img src="https://www.sneakpeek.com.ph/8355-big_default_2x/suede-classic-plus-black.jpg"></img>
+      </div>
+      <div class="Desc">
+        <p class="Category">{category}</p>
+        <h1 class="Title">{title}</h1>
+        <p class="Price">IDR {price} K</p>
+        <p class="Info">
+          Create React App doesn’t handle backend logic or databases; it just
+          creates a frontend build pipeline, so you can use it with any backend
+          you want. Under the hood, it uses Babel and webpack, but you don’t
+          need to know anything about them. When you’re ready to deploy to
+          production, running npm run build will create an optimized build of
+          your app in the build folder. You can learn more about Create React
+          App from its README and the User Guide.
+        </p>
+      </div>
     </div>
   );
 }
