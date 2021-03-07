6f9f8d804498773a962191fce4a17acf736c0888
Tommy Alhamra <tommy.alhamra@gmail.com>
2021-03-07 15:43:48 +0700

    update+ conditonal render

--- src/App.js
+++ src/App.js
@@ -7,9 +7,25 @@ function App() {
   return (
     <div className="ParentBox">
       <ProductImg />
-      <ProductInfo category="Special" title="Puma Black Classic" price="1500" />
+      <ProductInfo
+        isDiscount="yes"
+        category="Special"
+        title="Puma Black Classic"
+        price="1500"
+      />
     </div>
   );
+}
+
+function CheckDiscount(props) {
+  const { isDiscount } = props;
+  if (isDiscount === "yes") {
+    return <p>Diskon 50%</p>;
+  } else if (isDiscount === "comming") {
+    return <p>Comming soon</p>;
+  } else {
+    return <p>Belum ada diskon</p>;
+  }
 }
 
 function ProductImg() {
@@ -21,23 +37,19 @@ function ProductImg() {
 }
 
 function ProductInfo(props) {
-  const { category } = props;
-  const { title } = props;
-  const { price } = props;
+  const { category, title, price, isDiscount } = props;
 
   return (
     <div className="Desc">
       <p className="Category">{category}</p>
       <h1 className="Title">{title}</h1>
       <p className="Price">IDR {price} K</p>
+      <CheckDiscount isDiscount={isDiscount} />
       <p className="Info">
         Create React App doesn’t handle backend logic or databases; it just
         creates a frontend build pipeline, so you can use it with any backend
         you want. Under the hood, it uses Babel and webpack, but you don’t need
         to know anything about them. When you’re ready to deploy to production,
-        running npm run build will create an optimized build of your app in the
-        build folder. You can learn more about Create React App from its README
-        and the User Guide.
       </p>
       <a onClick={(i) => addToCart(title, i)} href="#">
         Add to Cart
