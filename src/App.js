55e01357151917f0eb56e8a0839a596d47cdf80c
Tommy Alhamra <tommy.alhamra@gmail.com>
2021-03-07 19:17:07 +0700

    add+ card testi and play with data json

--- src/App.js
+++ src/App.js
@@ -13,6 +13,50 @@ function App() {
         title="Puma Black Classic"
         price="1500"
       />
+      <ReviewItems />
+    </div>
+  );
+}
+
+function ReviewItems() {
+  const users = [
+    {
+      id: 1,
+      name: "Yessi Hutagaol",
+      review: "Barangnya sesuai gambaar, kualitas jangan ditanya",
+      img:
+        "https://images.pexels.com/photos/1987301/pexels-photo-1987301.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    },
+    {
+      id: 2,
+      name: "Paramansyah H",
+      review: "Gak nyesel gue beli ini. asyoy banget",
+      img:
+        "https://images.pexels.com/photos/2379004/pexels-photo-2379004.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    },
+    {
+      id: 3,
+      name: "Curzhika Yuandari",
+      review: "Aku suka banget sepatunya.. makasih sayang",
+      img:
+        "https://images.pexels.com/photos/6941834/pexels-photo-6941834.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    },
+  ];
+
+  const dataUsers = users.map((data) => (
+    <div className="Item">
+      <img src={data.img}></img>
+      <div className="User">
+        <h3>{data.name}</h3>
+        <p>{data.review}</p>
+      </div>
+    </div>
+  ));
+
+  return (
+    <div className="Review-box">
+      <h2>Review</h2>
+      {dataUsers}
     </div>
   );
 }
